beebaf8c4bbbde7453289278a5d6d7368093a1ad
Lee Soyeon <data.sy.2@gmail.com>
2023-08-21 21:33:37 +0900

    Arrays 메서드 보는 중

---
+++ java/GrammersForCodingTest/src/no04_Arrays/Ex01_배열생성.java
@@ -0,0 +1,32 @@
+package no04_Arrays;
+
+import java.util.StringTokenizer;
+
+public class Ex01_배열생성 {
+
+	public static void main(String[] args) {
+		
+		// 배열 : "같은 타입"의 여러 변수를 하나의 묶음으로 다룸
+			// 순차적 => 인덱스
+			// 길이 고정
+			// 주소를 참조 => 콜 바이 레퍼런스
+		
+		// 선언 & 할당
+		//         ↓ 대괄호는 배열을 의미
+		// 데이터타입[ ] 변수명 = new 데이터타입[배열크기];
+		//    (좌변) 선언          (우변) 할당
+		int[] arr = new int[3];
+		arr[0] = 10;
+		arr[1] = 20;
+		arr[2] = 30;
+		//                   ↓ 중괄호 사용해서 바로 값 넣기
+		// 데이터타입[ ] 변수명 = {ㅇ, ㅇ, ㅇ};		
+		String[] arr2 = {"Hello", "Java", "World"};
+		
+		
+		// 각 원소에 접근하기(값 넣기, 출력하기 등) : for문 돌려서 arr[i]
+		for(int i=0; i<arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+}
---
+++ java/GrammersForCodingTest/src/no04_Arrays/Ex02_메서드.java
@@ -0,0 +1,36 @@
+package no04_Arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Ex02_메서드 {
+
+	public static void main(String[] args) {
+
+		int[] arr = {100, 10, 1000, 1, 10000};
+		String[] arrStr = {"Hello", "Java", "World"};
+
+		System.out.println("===== toString : String으로 변환하는 건데 print에 for문 안 돌려도 되는 용법으로 사용 =====");
+		System.out.println(Arrays.toString(arr)); // [100, 10, 1000, 1, 10000]
+		
+		System.out.println("===== sort : 오름차순 정렬 =====");
+		Arrays.sort(arr);
+		System.out.println(Arrays.toString(arr)); // [1, 10, 100, 1000, 10000]		
+		System.out.println("===== binarySearch : (정렬된 배열에 대해) 이진탐색 =====");
+		System.out.println(Arrays.binarySearch(arr, 10)); // 1
+
+		System.out.println("===== copyOf(A, n) : A를 길이 n인 배열로 복사 =====");
+		String[] arrStr2 = Arrays.copyOf(arrStr, 5);
+		System.out.println(Arrays.toString(arrStr2)); // 남는 길이는 기본값으로 초기화
+		System.out.println("===== copyOfRange(A, n, m) : A를 인덱스 n이상 m미만 슬라이싱해서 복사 =====");
+		int[] arr2 = Arrays.copyOfRange(arr, 1, 4);
+		System.out.println(Arrays.toString(arr2)); // [10, 100, 1000]
+	
+		System.out.println("===== fill(A, a) : 배열 A를 특정값 a로 초기화 =====");
+		Arrays.fill(arr, 5);
+		System.out.println(Arrays.toString(arr)); // [5, 5, 5, 5, 5]
+		
+	}
+
+}
