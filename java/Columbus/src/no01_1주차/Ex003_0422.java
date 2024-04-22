c2fd7d2efbc9e88b602fa268442bfecf21649d55
Lee Soyeon <data.sy.2@gmail.com>
2024-04-22 23:59:12 +0900

    백준 10798
    - char 초기화하면 null이 들어 있고, 배열 출력할 때 null 따로 처리해줘야 함]
    - max 크기가 정해져 있으면 굳이 데이터 별 max 찾지 말고, 그냥 최대 크기로 사용

---
+++ java/Columbus/src/no01_1주차/Ex003_0422.java
@@ -0,0 +1,37 @@
+package no01_1주차;
+
+import java.io.OutputStreamWriter;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class Ex003_0422 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		// 조건에서 주어진 최대값 15를 바로 사용하면 max를 찾을 필요 없어
+		char[][] arr = new char[15][5];
+		for (int i=0; i<5; i++) {
+			String str = br.readLine();
+			for (int j=0; j<str.length(); j++) {
+				arr[j][i] = str.charAt(j);
+			}
+		}
+		for (int i=0; i<15; i++) {
+			bw.write(String.valueOf(arr[i]));
+			// chatGPT도 여기서 null인 열은 날린다고 했는데... 아닌가봐..;;
+			// Stirng.valueOf에 대해 더 공부해보기 
+		}
+		
+		bw.flush();
+		
+		br.close();
+		bw.close();
+
+	}
+
+}
---
+++ java/Columbus/src/no01_1주차/Ex003_0422_sol.java
@@ -0,0 +1,40 @@
+package no01_1주차;
+
+import java.io.OutputStreamWriter;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class Ex003_0422_sol {
+
+	public static void main(String[] args) throws IOException{
+		
+		// null은 콘솔창에서 안 보일 뿐 데이터 안에는 들어있어서 계속 틀렸던 것!
+		// https://www.acmicpc.net/board/view/112680
+		// https://www.acmicpc.net/board/view/121271
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		char[][] arr = new char[5][15];
+		for (int i=0; i<5; i++) {
+			String str = br.readLine();
+			for (int j=0; j<str.length(); j++) {
+				arr[i][j] = str.charAt(j);
+			}
+		}
+		for (int j=0; j<15; j++) {
+			for (int i=0; i<5; i++) {
+				if(arr[i][j] == (char) 0) continue;
+				bw.write(String.valueOf(arr[i][j]));		
+		}}
+		
+		bw.flush();
+		
+		br.close();
+		bw.close();
+
+	}
+
+}
