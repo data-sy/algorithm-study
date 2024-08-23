ae725a9c7a5abe48e995b3717c9ea8fd397761a9
Lee Soyeon <data.sy.2@gmail.com>
2024-08-23 19:32:05 +0900

    반복문 변수값 테스트

--- java/CoTeStudy/src/Main.java
+++ java/CoTeStudy/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+
+        for (int i=3; i<2; i++) {
+            System.out.println("들어오나?");
+        }
         System.out.println("Hello world!");
     }
 }
--- java/CoTeStudy/src/no01_카카오기출/Ex004_두큐합같게만들기_0815.java
+++ java/CoTeStudy/src/no01_카카오기출/Ex004_두큐합같게만들기_0815.java
@@ -1,6 +1,5 @@
 package no01_카카오기출;
 
-import java.io.IOException;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -17,7 +16,7 @@ public class Ex004_두큐합같게만들기_0815 {
 //		int[] queue1 = {1, 1};
 //		int[] queue2 = {1, 5};
 
-		// 임계점 2n+2에 대한 테스트 케이스
+		// 임계점 2n+2에 대한 반례
 		int[] queue1 = {1, 1, 1, 1, 1, 1};
 		int[] queue2 = {1, 1, 1, 1, 11, 1};
 
@@ -34,25 +33,20 @@ public class Ex004_두큐합같게만들기_0815 {
 			q2.add(queue2[i]);
 			q2Sum += queue2[i];
 		}
-		long check = 0;
 
-		if ((q1Sum+q2Sum)%2==0) {
-			check = (q1Sum+q2Sum)/2;
-		} else answer=-1; // 프로그래머스에서 리턴으로 바꿔
+		if ((q1Sum+q2Sum)%2!=0) answer=-1;
 
-		while(q1Sum!=check){
+		while(q1Sum!=q2Sum){
 			if (q1Sum>q2Sum) {
 				int tmp = q1.poll();
 				q1Sum-=tmp;
 				q2Sum+=tmp;
 				q2.add(tmp);
-			} else if (q1Sum<q2Sum) {
+			} else { // cf. else로만 해도 됨! == 인 경우는 while에서 이미 점검
 				int tmp = q2.poll();
 				q2Sum-=tmp ;
 				q1Sum+=tmp;
 				q1.add(tmp);
-			} else {
-				// 리턴 answer
 			}
 			answer++;
 			if (answer>2*n+2) { // 끝까지 다 돌려봤을 때
