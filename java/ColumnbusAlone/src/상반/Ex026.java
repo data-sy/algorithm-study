bc9f28b3cd55a855632a11750b7748b4cf5c7e53
Lee Soyeon <data.sy.2@gmail.com>
2025-01-13 17:52:07 +0900

    백준 2960

---
+++ java/ColumnbusAlone/src/상반/Ex026.java
@@ -0,0 +1,47 @@
+package 상반;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex026 {
+
+    public static void main(String[] args) throws IOException {
+        // https://www.acmicpc.net/problem/2960
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        boolean[] isCompositeArr = new boolean[N+1]; // 합성수면 T, 소수이면 F
+        int num = 0;
+        boolean isFind = false;
+        for (int i = 2; i <= N; i++) {
+            if (!isCompositeArr[i]) {
+                K--;
+                if (K == 0) {
+                    num = i;
+                    break;
+                }
+                for (int j = 2 * i; j <= N; j += i) {
+                    if (!isCompositeArr[j]) {
+                        isCompositeArr[j] = true;
+                        K--;
+                    }
+                    if (K == 0) {
+                        num = j;
+                        isFind = true;
+                        break;
+                    }
+                }
+                if (isFind) break;
+            }
+        }
+        System.out.println(num);
+
+    }
+
+}
