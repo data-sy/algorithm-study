ec776c45fca4caf93996578c42b9757af7c17dbd
Lee Soyeon <data.sy.2@gmail.com>
2026-03-30 18:32:09 +0900

    feat: add problem skeleton files for coding test prep
    - Add 52 Java skeleton files in 상반2604 with BOJ problem links
    - Each file contains only package, imports, and problem URL for self-solving

---
+++ java/ColumnbusAlone/src/상반2604/Ex055.java
@@ -0,0 +1,11 @@
+package 상반2604;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex055 {
+    public static void main(String[] args) throws IOException {
+        // https://www.acmicpc.net/problem/1541
+    }
+}
