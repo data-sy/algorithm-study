f6b2580b3af804a740dd4a36a9618740aefb92b0
Lee Soyeon <data.sy.2@gmail.com>
2026-04-01 16:05:20 +0900

    feat: solve BG11720, BG9046, BG10798 problems
    - Implement Ex001 digit sum solution using split and parseInt
    - Replace Ex002 skeleton with Ex002_1 array-based frequency counting solution
    - Add Ex002_2 (Map approach, WIP) and Ex002_3 (Stream approach, WIP) variants
    - Implement Ex003 vertical reading with 2D char array and padding
    - Add local claude settings for java subdirectories

--- java/programmers/Ex001_BG11720.java
+++ java/programmers/Ex001_BG11720.java
@@ -3,10 +3,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Ex001_BG11720 {
 
     public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        int N = Integer.parseInt(line);
+        line = br.readLine();
+        String[] arr = line.split("");
+
+        int sum = 0;
+        for (int i = 0; i < N ; i++) {
+            sum += Integer.parseInt(arr[i]);
+        }
+        System.out.println(sum);
 
     }
 
---
+++ java/programmers/Ex002_3_BG9046_스트림공부후.java
@@ -0,0 +1,23 @@
+// 백준: https://www.acmicpc.net/problem/9046
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex002_3_BG9046_스트림공부후 {
+
+    public static void main(String[] args) throws IOException {
+        // 방법3: 스트림 사용도 가능할까? 고민해보자
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i <T; i++) {
+            String str = br.readLine();
+
+        }
+
+    }
+
+}
